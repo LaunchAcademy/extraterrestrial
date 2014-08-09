b188af803bafbfbbd2680de4a8c0beb8fa7f34a8
Adam Sheehan <a.t.sheehan@gmail.com>
2014-08-09 16:42:32 -0400

    Store credentials in a config file.

--- lib/et/runner.rb
+++ lib/et/runner.rb
@@ -1,17 +1,39 @@
+require "gli"
+require "yaml"
+
 module ET
   class Runner
-    SUCCESS = 0
+    extend GLI::App
+    version VERSION
 
-    def self.go(_args)
-      api = API.new("http://localhost:3000")
-      results = api.list_challenges
+    def self.go(args, cwd = Dir.pwd)
+      desc "Initialize current directory as challenge work area."
+      command :init do |c|
+        c.action do |_global_options, _options, args|
+          config = {
+            "username" => args[0],
+            "token" => args[1],
+            "host" => args[2]
+          }
 
-      results[:challenges].each do |challenge|
-        puts challenge[:title]
-        puts challenge[:slug]
+          File.write(File.join(cwd, ".et"), config.to_yaml)
+        end
       end
 
-      SUCCESS
+      desc "List available challenges."
+      command :challenges do |c|
+        c.action do |_global_options, _options, _args|
+          api = API.new("http://localhost:3000")
+          results = api.list_challenges
+
+          results[:challenges].each do |challenge|
+            puts challenge[:title]
+            puts challenge[:slug]
+          end
+        end
+      end
+
+      run(args)
     end
   end
 end
---
+++ spec/cli/init_spec.rb
@@ -0,0 +1,21 @@
+require "json"
+
+describe "init" do
+  it "writes the user credentials to a config file" do
+    Dir.mktmpdir("test") do |tmpdir|
+      arguments = ["init", "barry", "foobar", "http://localhost:3000"]
+
+      _, _ = capture_output do
+        expect(ET::Runner.go(arguments, tmpdir)).to eq(0)
+      end
+
+      filepath = File.join(tmpdir, ".et")
+      expect(File.exist?(filepath)).to eq(true)
+
+      config = YAML.load(File.read(filepath))
+      expect(config["username"]).to eq("barry")
+      expect(config["token"]).to eq("foobar")
+      expect(config["host"]).to eq("http://localhost:3000")
+    end
+  end
+end
