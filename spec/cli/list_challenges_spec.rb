e9a6d3d301d5feec8ac9a6c11db0d06a0e617d73
Adam Sheehan <a.t.sheehan@gmail.com>
2014-08-09 15:58:40 -0400

    Move test code out of module.

--- spec/cli/list_challenges_spec.rb
+++ spec/cli/list_challenges_spec.rb
@@ -1,27 +1,25 @@
-module ET
-  describe "list challenges" do
-    let(:sample_challenges) do
-      {
-        challenges: [
-          { title: "Guess the Number", slug: "guess-the-number" },
-          { title: "Blackjack", slug: "blackjack" }
-        ]
-      }
+describe "list challenges" do
+  let(:sample_challenges) do
+    {
+      challenges: [
+        { title: "Guess the Number", slug: "guess-the-number" },
+        { title: "Blackjack", slug: "blackjack" }
+      ]
+    }
+  end
+
+  it "prints the titles and slug" do
+    expect_any_instance_of(ET::API).to receive(:list_challenges).
+      and_return(sample_challenges)
+
+    stdout, _ = capture_output do
+      expect(ET::Runner.go(["challenges"])).to eq(0)
     end
 
-    it "prints the titles and slug" do
-      expect_any_instance_of(API).to receive(:list_challenges).
-        and_return(sample_challenges)
+    expect(stdout).to include("Guess the Number")
+    expect(stdout).to include("guess-the-number")
 
-      stdout, _ = capture_output do
-        expect(Runner.go(["challenges"])).to eq(0)
-      end
-
-      expect(stdout).to include("Guess the Number")
-      expect(stdout).to include("guess-the-number")
-
-      expect(stdout).to include("Blackjack")
-      expect(stdout).to include("blackjack")
-    end
+    expect(stdout).to include("Blackjack")
+    expect(stdout).to include("blackjack")
   end
 end
--- spec/lib/api_spec.rb
+++ spec/lib/api_spec.rb
@@ -1,23 +1,21 @@
-module ET
-  describe API do
-    let(:api) { API.new("http://localhost:3000") }
+describe ET::API do
+  let(:api) { ET::API.new("http://localhost:3000") }
 
-    describe "challenges" do
-      let(:challenges_response) do
-        File.read("spec/data/challenges.json")
-      end
+  describe "challenges" do
+    let(:challenges_response) do
+      File.read("spec/data/challenges.json")
+    end
 
-      it "queries for a list of challenges" do
-        expect(RestClient).to receive(:get).
-          with("http://localhost:3000/challenges.json").
-          and_return(challenges_response)
+    it "queries for a list of challenges" do
+      expect(RestClient).to receive(:get).
+        with("http://localhost:3000/challenges.json").
+        and_return(challenges_response)
 
-        results = api.list_challenges
+      results = api.list_challenges
 
-        expect(results[:challenges].count).to eq(2)
-        expect(results[:challenges][0][:title]).to eq("Auto-Guesser")
-        expect(results[:challenges][0][:slug]).to eq("auto-guesser")
-      end
+      expect(results[:challenges].count).to eq(2)
+      expect(results[:challenges][0][:title]).to eq("Auto-Guesser")
+      expect(results[:challenges][0][:slug]).to eq("auto-guesser")
     end
   end
 end
